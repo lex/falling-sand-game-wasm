1a8b3a443965e3d1c0742ce04be93b6182b59b2b
lex <lex@users.noreply.github.com>
2020-05-03 16:54:29 +0300

    helloworld -> game

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -22,9 +22,7 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import type { SandGame } from "../../pkg/index";
 
 @Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-
+export default class Game extends Vue {
   //eslint-disable-next-line
   private wasm!: any;
   private sandGame!: SandGame;
@@ -103,18 +101,4 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Game />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Game from "@/components/Game.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Game
   }
 };
 </script>
