707e864a0fceb872cae190879ee46a065f77d6b3
lex <lex@users.noreply.github.com>
2020-05-03 21:39:42 +0300

    clean up renderer.rs a bit more

--- rust/src/renderer.rs
+++ rust/src/renderer.rs
@@ -41,20 +41,107 @@ impl Renderer {
         let program_info = self.create_program().unwrap();
         self.program_info = Some(program_info);
 
-
         let buffers = self.create_buffers().unwrap();
         self.buffers = Some(buffers);
 
         Ok(())
     }
 
+    pub fn render(&self, framebuffer: &[u8], width: u32, height: u32) {
+        let context = self.context.as_ref().unwrap();
+        let buffers = self.buffers.as_ref().unwrap();
+        let program_info = self.program_info.as_ref().unwrap();
+
+        let vertex_buffer = buffers.vertex_buffer.as_ref().unwrap();
+        let index_buffer = buffers.index_buffer.as_ref().unwrap();
+        let texture_coordinate_buffer = buffers.texture_coordinate_buffer.as_ref().unwrap();
+        let texture = buffers.texture.as_ref().unwrap();
+
+        let a_vertex_position = program_info.a_vertex_position;
+        let a_texture_coordinate = program_info.a_texture_coordinate;
+        let u_sampler = program_info.u_sampler.as_ref();
+
+        // vertex buffer
+        context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
+        context.vertex_attrib_pointer_with_i32(
+            a_vertex_position as u32,
+            2,
+            WebGlRenderingContext::FLOAT,
+            false,
+            0,
+            0,
+        );
+
+        context.enable_vertex_attrib_array(a_vertex_position as u32);
+
+        // indices
+        context.bind_buffer(
+            WebGlRenderingContext::ELEMENT_ARRAY_BUFFER,
+            Some(&index_buffer),
+        );
+
+        // texture coordinates
+        context.bind_buffer(
+            WebGlRenderingContext::ARRAY_BUFFER,
+            Some(&texture_coordinate_buffer),
+        );
+
+        context.vertex_attrib_pointer_with_i32(
+            a_texture_coordinate as u32,
+            2,
+            WebGlRenderingContext::FLOAT,
+            false,
+            0,
+            0,
+        );
+
+        context.enable_vertex_attrib_array(a_texture_coordinate as u32);
+
+        // texture
+        context.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&texture));
+        context.active_texture(WebGlRenderingContext::TEXTURE0);
+
+        let _result = context
+            .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+                WebGlRenderingContext::TEXTURE_2D,
+                0,
+                WebGlRenderingContext::RGB as i32,
+                width as i32,
+                height as i32,
+                0,
+                WebGlRenderingContext::RGB,
+                WebGlRenderingContext::UNSIGNED_BYTE,
+                Some(framebuffer),
+            );
+
+        // uniforms
+        if let Some(u) = &u_sampler {
+            context.uniform1i(Some(u), 0);
+        }
+
+        // clear
+        context.clear_color(0.0, 0.0, 0.0, 1.0);
+        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+        // draw
+        let indices_len = 6;
+        context.draw_elements_with_i32(
+            WebGlRenderingContext::TRIANGLES,
+            indices_len as i32,
+            WebGlRenderingContext::UNSIGNED_BYTE,
+            0,
+        );
+    }
+}
+
+impl Renderer {
     fn create_buffers(&self) -> Result<Buffers, String> {
         let context = self.context.as_ref().unwrap();
         let vertices: [f32; 8] = [
             -1.0, 1.0, // upper left
             -1.0, -1.0, // lower left
-            1.0, -1.0,  // lower right
-            1.0, 1.0  // upper right
+            1.0, -1.0, // lower right
+            1.0, 1.0, // upper right
         ];
 
         let indices: [u8; 6] = [3, 2, 1, 3, 1, 0];
@@ -124,22 +211,42 @@ impl Renderer {
 
         let default_texture: [u8; 3] = [0, 0, 0];
 
-        let _result = context.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+        let _result = context
+            .tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
+                WebGlRenderingContext::TEXTURE_2D,
+                0,
+                WebGlRenderingContext::RGB as i32,
+                1,
+                1,
+                0,
+                WebGlRenderingContext::RGB,
+                WebGlRenderingContext::UNSIGNED_BYTE,
+                Some(&default_texture),
+            );
+
+        context.tex_parameteri(
             WebGlRenderingContext::TEXTURE_2D,
-            0,
-            WebGlRenderingContext::RGB as i32,
-            1,
-            1,
-            0,
-            WebGlRenderingContext::RGB,
-            WebGlRenderingContext::UNSIGNED_BYTE,
-            Some(&default_texture)
+            WebGlRenderingContext::TEXTURE_MIN_FILTER,
+            WebGlRenderingContext::NEAREST as i32,
         );
 
-        context.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_MIN_FILTER, WebGlRenderingContext::NEAREST as i32);
-        context.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_MAG_FILTER, WebGlRenderingContext::NEAREST as i32);
-        context.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_WRAP_S, WebGlRenderingContext::CLAMP_TO_EDGE as i32);
-        context.tex_parameteri(WebGlRenderingContext::TEXTURE_2D, WebGlRenderingContext::TEXTURE_WRAP_T, WebGlRenderingContext::CLAMP_TO_EDGE as i32);
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_MAG_FILTER,
+            WebGlRenderingContext::NEAREST as i32,
+        );
+
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_WRAP_S,
+            WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+        );
+
+        context.tex_parameteri(
+            WebGlRenderingContext::TEXTURE_2D,
+            WebGlRenderingContext::TEXTURE_WRAP_T,
+            WebGlRenderingContext::CLAMP_TO_EDGE as i32,
+        );
 
         let buffers = Buffers {
             vertex_buffer: Some(vertex_buffer),
@@ -149,77 +256,6 @@ impl Renderer {
         };
 
         return Ok(buffers);
-    }
-
-    pub fn render(&self, framebuffer: &[u8], width: u32, height: u32) {
-        let context = self.context.as_ref().unwrap();
-        let buffers = self.buffers.as_ref().unwrap();
-        let program_info = self.program_info.as_ref().unwrap();
-
-        let vertex_buffer = buffers.vertex_buffer.as_ref().unwrap();
-        let index_buffer = buffers.index_buffer.as_ref().unwrap();
-        let texture_coordinate_buffer = buffers.texture_coordinate_buffer.as_ref().unwrap();
-        let texture = buffers.texture.as_ref().unwrap();
-
-        let vertex_position = program_info.a_vertex_position;
-        let texture_coordinate = program_info.a_texture_coordinate;
-        let u_sampler  = program_info.u_sampler.as_ref();
-
-        context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
-        context.vertex_attrib_pointer_with_i32(
-            vertex_position as u32,
-            2,
-            WebGlRenderingContext::FLOAT,
-            false,
-            0,
-            0,
-        );
-        context.enable_vertex_attrib_array(vertex_position as u32);
-
-        // indices
-        context.bind_buffer(WebGlRenderingContext::ELEMENT_ARRAY_BUFFER, Some(&index_buffer));
-
-        // texture coordinates
-        context.bind_buffer(
-            WebGlRenderingContext::ARRAY_BUFFER,
-            Some(&texture_coordinate_buffer),
-        );
-        context.vertex_attrib_pointer_with_i32(
-            texture_coordinate as u32,
-            2,
-            WebGlRenderingContext::FLOAT,
-            false,
-            0,
-            0,
-        );
-        context.enable_vertex_attrib_array(texture_coordinate as u32);
-
-        context.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&texture));
-
-        let _result = context.tex_image_2d_with_i32_and_i32_and_i32_and_format_and_type_and_opt_u8_array(
-            WebGlRenderingContext::TEXTURE_2D,
-            0,
-            WebGlRenderingContext::RGB as i32,
-            width as i32,
-            height as i32,
-            0,
-            WebGlRenderingContext::RGB,
-            WebGlRenderingContext::UNSIGNED_BYTE,
-            Some(framebuffer),
-        );
-
-        context.clear_color(0.0, 0.0, 0.0, 1.0);
-        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
-
-        context.active_texture(WebGlRenderingContext::TEXTURE0);
-        context.bind_texture(WebGlRenderingContext::TEXTURE_2D, Some(&texture));
-
-        if let Some(u) = &u_sampler {
-            context.uniform1i(Some(u), 0);
-        }
-
-        let indices_len = 6;
-        context.draw_elements_with_i32(WebGlRenderingContext::TRIANGLES, indices_len as i32, WebGlRenderingContext::UNSIGNED_BYTE, 0);
     }
 
     fn create_program(&self) -> Result<ProgramInfo, String> {
